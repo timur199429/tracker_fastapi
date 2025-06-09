8a14d56e591530597be44eda12df8cf4e1ffb182
Timur Aliyev <timur199429@gmail.com>
2025-06-09 16:42:08 +0300

    убрал if __name__

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ async def submit_contact(form: ContactForm):
 
     return {"status": "ok", "message": "Контакт получен"}
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
